9531ceff9c5b54bf4d5294740191f6acc72420b9
wakandee <koechallanhi@gmail.com>
2024-07-20 14:18:09 +0300

    fix my code changes

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -8,12 +8,10 @@ from flask import Flask, jsonify, make_response
 app = Flask(__name__)
 app.register_blueprint(app_views)
 
-
 @app.errorhandler(404)
 def not_found(error):
-    """ json 404 page """
+    """ JSON 404 page """
     return make_response(jsonify({"error": "Not found"}), 404)
-
 
 if __name__ == "__main__":
     # python -m api.v1.app 
--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -2,9 +2,7 @@
 """ Index view
 """
 from flask import jsonify
-
 from api.v1.views import app_views
-
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
