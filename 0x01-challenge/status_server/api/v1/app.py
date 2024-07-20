218f00d77a68bcdd6f874227ac7b91e595fa06a6
wakandee <koechallanhi@gmail.com>
2024-07-20 14:32:04 +0300

    changing the index and init order

--- 0x01-challenge/status_server/api/v1/app.py
+++ 0x01-challenge/status_server/api/v1/app.py
@@ -2,8 +2,9 @@
 """
 Web server 
 """
-from api.v1.views import app_views
 from flask import Flask, jsonify, make_response
+from api.v1.views import app_views
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
--- 0x01-challenge/status_server/api/v1/views/index.py
+++ 0x01-challenge/status_server/api/v1/views/index.py
@@ -2,7 +2,9 @@
 """ Index view
 """
 from flask import jsonify
+
 from api.v1.views import app_views
+
 
 @app_views.route('/api/v1/status', methods=['GET'], strict_slashes=False)
 def status():
