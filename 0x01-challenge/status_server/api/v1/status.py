d85d2e34973a936a2e69979b60905d0bd8d46aa6
wakandee <koechallanhi@gmail.com>
2024-07-20 13:49:14 +0300

    removed status file

--- 0x01-challenge/status_server/api/v1/status.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, jsonify
-
-app_views = Blueprint('app_views', __name__)
-
-@app_views.route('/api/v1/status', methods=['GET'])
-def status():
-    return jsonify({"status": "OK"})
