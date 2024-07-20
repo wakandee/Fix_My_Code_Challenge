c995a73d74ca7b5d7a6db5ae516be769e00ab171
wakandee <koechallanhi@gmail.com>
2024-07-20 15:26:34 +0300

    square additionl changes

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,22 +2,24 @@
 
 class Square:
     def __init__(self, width=0, height=0):
+        # Initialize the square with width and height
         self.width = width
         self.height = height
 
     def area(self):
-        """ Return the area of the square """
+        """Return the area of the square"""
         if self.width <= 0 or self.height <= 0:
             return 0
         return self.width * self.height
 
     def perimeter(self):
-        """ Return the perimeter of the square """
+        """Return the perimeter of the square"""
         if self.width <= 0 or self.height <= 0:
             return 0
         return 2 * (self.width + self.height)
 
     def __str__(self):
+        # Return the string representation of the square
         return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
